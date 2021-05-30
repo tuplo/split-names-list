26134ae597a2fdb0ec0421645cef4952256b0677
Rui Costa <ruicosta042@gmail.com>
2021-05-30 21:14:27 +0100

    feat: include commonjs file in package

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ tsc --build tsconfig.build.json
 
 esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs
 esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js
+cp src/split-names-list.d.ts dist/split-names-list.d.ts
 
 # node12 compatibility
 mkdir cjs && cp dist/index.cjs cjs/index.js
