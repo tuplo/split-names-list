93de0175e57fb41502693e5d32971673a947ca12
Rui Costa <ruicosta042@gmail.com>
2024-01-05 16:42:48 +0000

    chore: update deps, security fixes

--- sh/coverage.ts
+++ sh/coverage.ts
@@ -1,6 +1,8 @@
-import { $ } from "@tuplo/shell";
+import * as shell from "@tuplo/shell";
 
 async function main() {
+	const $ = shell.$({ verbose: true });
+
 	await $`rm -rf ./node_modules/.cache`;
 	await $`rm -rf coverage/`;
 	await $`rm -rf .nyc_output/`;
